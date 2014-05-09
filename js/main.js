7c788363740cab45d1481c2b0f23c1d941d57920
Norm Glomski <nglomski@gmail.com>
2014-05-09 18:14:58 -0500

    pre-save new accountability item

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-
+function init_pop_links(){
+    $('.pop-link').magnificPopup({type:'image'});
+    //alert('pop-links init complete.');
+};
--- scripts/posts-controllers.js
+++ scripts/posts-controllers.js
@@ -40,9 +40,45 @@ angular.module('updaterApp').controller('PostsDetailController', ['$scope', '$ht
     $http.post($scope.api_url, $routeParams).success(function(data, status, headers){
         if(status == "200"){
             $scope.post = data[0];
+            init_pop_links();
+            /*
+            $scope.modalShown = false;
+            $scope.toggleModal = function() {
+              	$scope.modalShown = !$scope.modalShown;
+            }; 
+            //http://jsfiddle.net/houston88/ab23r/1/
+            angular.forEach($scope.post.blocks, function(block, index) {
+              if (block.pb_image_01) {
+                console.log('Need to add modal for block '+index);              
+              }
+            });
+            */
         }
-    });
+    });  
 }]);
+/*
+//Image Lightbox Modal
+updaterApp.directive('modalDialog', function() {
+	return {
+    restrict: 'A',
+    scope: { show: '=' },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+        scope.dialogStyle = {};
+        if (attrs.width)
+          scope.dialogStyle.width = attrs.width;
+        if (attrs.height)
+          scope.dialogStyle.height = attrs.height;
+        scope.hideModal = function() {
+          scope.show = false;
+        };
+      },
+      template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+    };
+});
+*/
+
 
 //Youtube Directive
 updaterApp.directive('myYoutube', function($sce) {
